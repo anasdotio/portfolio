255ee626fe928e5db2ec3947be730de7bc32701c
Anas <anasroyal385@gmail.com>
2026-02-04 20:13:59 +0530

    update navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,46 @@ import React from "react";
 
 const Navbar = () => {
   const navItems = ["Home", "About", "Skills", "Projects", "Contact"];
+
   return (
-    <div className="w-fit mx-auto bg-linear-to-r from-white via-black/20 to-white p-[.6px] mt-3 rounded-md">
-      <div className="flex gap-8 bg-black p-2 rounded-md">
+    <div className="w-fit mx-auto mt-6">
+      <div className="flex gap-8 bg-black/80 px-6 py-3 rounded-md">
         {navItems.map((item) => (
-          <p key={item}>{item}</p>
+          <div
+            key={item}
+            className="
+              relative 
+              h-6 
+              min-w-fit 
+              overflow-hidden 
+              cursor-pointer 
+              group
+              text-white
+            "
+          >
+            {/* Normal Text */}
+            <span
+              className="
+                block
+                transition-transform duration-300
+                group-hover:-translate-y-full
+              "
+            >
+              {item}
+            </span>
+
+            {/* Hover Text */}
+            <span
+              className="
+                block
+                text-orange-400
+                transition-transform duration-300
+                group-hover:-translate-y-full
+              "
+            >
+              {item}
+            </span>
+          </div>
         ))}
       </div>
     </div>
